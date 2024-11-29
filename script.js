2bfeeebf2a7262d2f108644623cace4a078c2217
Anthony Stanislas <103941632+Wixtil@users.noreply.github.com>
2024-11-29 11:18:38 +0100

    ajout script + csv

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 fetch(
-    `https://query.wikidata.org/sparql?query=SELECT%20?Iut%20?IutLabel%20?localisation_administrative%20?localisation_administrativeLabel%20?codeInsee%20WHERE%20{%20%20%20SERVICE%20wikibase:label%20{%20bd:serviceParam%20wikibase:language%20"[AUTO_LANGUAGE],mul,en".%20}%20%20%20?Iut%20wdt:P31%20wd:Q3152659;%20%20%20%20%20wdt:P17%20wd:Q142.%20%20%20OPTIONAL%20{%20?Iut%20wdt:P131%20?localisation_administrative.%20?localisation_administrative%20wdt:P374%20?codeInsee.}%20}%20LIMIT%2010000&format=json`
+    `Microplastic.csv`
   ).then((d) =>
     d.json().then(function (data) {
       console.log(data);
