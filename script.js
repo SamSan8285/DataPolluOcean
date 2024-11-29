6e1c973c1f36170f9717e62fc8c06ae6cd7658d2
Anthony Stanislas <103941632+Wixtil@users.noreply.github.com>
2024-11-29 12:31:37 +0100

    Changement css et script

--- script.js
+++ script.js
@@ -5,7 +5,16 @@ fetch(
       console.log(data);
 
 
-    const donnees = data.attributes;
+    const donnees = data.layers[0].features;
+    const donnesTrier = [];
+    donnees.forEach(element => {
+        donnesTrier.push({Date : element.attributes.Date})
+        // Convertir la date timestamp date
+    });
+
     console.log(donnees);
+    console.log (donnesTrier)
+
+
     })
 );
